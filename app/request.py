5d661e88fe8b387ba09f4cc8834a6e7ef25dfd1e
chanaiagwata <chanai.agwata@student.moringaschool.com>
2022-05-01 23:06:38 +0300

    feat:add variables to configure_request function

--- app/request.py
+++ app/request.py
@@ -4,7 +4,7 @@ from .models import Sources,Articles
 # Getting api key
 api_key = None
 #Getting base url
-base_url = None
+sources_base_url = None
 #Getting the Sources base url
 source_base_url = None
 #Getting the everything base url
@@ -15,9 +15,13 @@ headlines_base_url = None
 
 
 # Functions that take in application instance and replaces the None values above with configuration objects
-def configure_reques(app):
-    global api_key,sources_base_url,articles_base_url
+def configure_request(app):
+    global api_key,sources_base_url,source_base_url,everything_base_url,headlines_base_url
     api_key  = app.config['NEWS_API_KEY']
+    sources_base_url = app.config['NEWS_BASE_SOURCES_URL']
+    source_base_url = app.config['NEWS_BASE_SOURCE_URL']
+    everything_base_url = app.config['NEWS_BASE_EVERYTHING_URL']
+    headlines_base_url = app.config['NEWS_BASE_HEADLINES_URL']
 
 
 
