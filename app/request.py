9a63027cbb42346a118194858a2e248f183d5c6a
chanaiagwata <chanai.agwata@student.moringaschool.com>
2022-05-02 00:27:47 +0300

    feat:will call api and process results

--- app/request.py
+++ app/request.py
@@ -1,3 +1,4 @@
+from concurrent.futures import process
 import urllib.request,json
 from .models import Sources,Articles
 
@@ -22,6 +23,47 @@ def configure_request(app):
     source_base_url = app.config['NEWS_BASE_SOURCE_URL']
     everything_base_url = app.config['NEWS_BASE_EVERYTHING_URL']
     headlines_base_url = app.config['NEWS_BASE_HEADLINES_URL']
+
+def get_news_sources(category):
+    '''
+    Function that gets json response to our url request
+    '''
+    get_news_url  = sources_base_url(category,api_key)
+    with urllib.request.urlopen(get_news_url) as url:
+        get_news_data = url.read()
+        get_news_response= json.loads(get_news_data)
+        
+        news_source_results = None
+        
+        if get_news_response['sources']:
+            news_source_results_list = get_news_response['sources']
+            news_source_results = process_results(news_source_results_list)
+            
+    return news_source_results
+
+#processing results
+def process_results(source_list):
+    '''
+    Function  that processes the news source result and transform them to a list of Objects
+    
+    Args:
+        source_list: A list of dictionaries that contain news source details
+    Returns:
+        news_source_results: A list of news source objects
+    '''
+
+    news_source_results = []
+    for source_item in source_list:
+        name = source_item.get('name')
+        description = source_item.get('description')
+        url = source_item.get('url')
+        
+        if name:
+            source_object = Sources(name,description,url)
+            news_source_results.append(source_object)
+    
+    return news_source_results
+    
 
 
 
