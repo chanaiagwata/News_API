35aae929243548ed15a2cf7b8c00fb4fdac654fd
chanaiagwata <chanai.agwata@student.moringaschool.com>
2022-05-02 18:39:22 +0300

    feat:adds functions that return article and headline page

--- app/main/views.py
+++ app/main/views.py
@@ -6,7 +6,9 @@ from ..models import Sources, Articles
 #views
 @main.route('/')
 def index():
-    
+    '''
+    View root page function that returns the index page and its data
+    '''
     #getting sources
     business_sources = get_news_sources('business')
     sports_sources = get_news_sources('sports')
@@ -17,7 +19,21 @@ def index():
     
     return render_template('index.html', title = title, business = business_sources, sports = sports_sources, technology  = technology_sources, entertainment = entertainment_sources)
 
+@main.route('/articles/<id>')
+def articles(id):
+    '''
+    view article page
+    '''
+    articles = get_allArticles(id)
+    return render_template("articles.html", id = id, articles = articles)
 
+@main.route('/headlines/<title>')  
+def articles(title):
+    '''
+    view headline page
+    '''
+    headlines = get_headlines(title)
+    return render_template('headlines.html', title = title, headlines = headlines)
     
 
 
