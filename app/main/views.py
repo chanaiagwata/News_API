d8e0c95afe3e45f9e36b8f9145f4fad05301f38e
chanaiagwata <chanai.agwata@student.moringaschool.com>
2022-05-02 18:05:41 +0300

    feat:add view root page function

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,24 @@
-from flask import render_template
+from flask import render_template,request,redirect,url_for
 from . import main
+from ..request import get_news_sources,get_allArticles,get_headlines
+from ..models import Sources, Articles
 
+#views
 @main.route('/')
 def index():
-    return '<h2>Hey There Chanai</h2>'
+    
+    #getting sources
+    business_sources = get_news_sources('business')
+    sports_sources = get_news_sources('sports')
+    technology_sources = get_news_sources('technology')
+    entertainment_sources = get_news_sources('entertainment')
+    
+    title = "News Break"
+    
+    return render_template('index.html', title = title, business = business_sources, sports = sports_sources, technology  = technology_sources, entertainment = entertainment_sources)
+
+
+    
+
+
+
