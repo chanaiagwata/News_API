47ad135efccfa565466268f20c5f25363de33378
chanaiagwata <chanai.agwata@student.moringaschool.com>
2022-05-03 03:52:14 +0300

    feat:adds source routing to the navbar

--- app/main/views.py
+++ app/main/views.py
@@ -21,13 +21,13 @@ def index():
     
     return render_template('index.html', title = title, business_sources = business_sources, sports_sources=sports_sources, technology_sources=technology_sources,entertainment_sources=entertainment_sources)
 
-# @main.route('/articles/<id>')
-# def articles(id):
-#     '''
-#     view article page
-#     '''
-#     articles = get_allArticles(id)
-#     return render_template("articles.html", id = id, articles = articles)
+@main.route('/articles/<id>')
+def articles(id):
+    '''
+    view article page
+    '''
+    articles = get_allArticles(id)
+    return render_template("articles.html", id = id, articles = articles)
 
 # @main.route('/headlines/<title>')  
 # def headlines(title):
